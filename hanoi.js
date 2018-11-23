a588c258eab7e209a2e59192189115c486513b84
Konrad Łopuszyński <konrad.lopuszynski@outlook.com>
2018-11-23 18:11:48 +0100

    create putOnNewRod function

--- hanoi.js
+++ hanoi.js
@@ -7,39 +7,55 @@ var currentRod = 0;
 
 function drawInitHanoi(){
 	document.getElementById("game").innerHTML = 
-	'<div class="rod" id="r1" onclick="myClick(1)"></div>'+
-	'<div class="rod" id="r2" onclick="myClick(2)"></div>'+
-	'<div class="rod" id="r3" onclick="myClick(3)"></div>';
+	'<div class="rod" id="r1" onclick="myClick(1)">'+stack1+'</div>'+
+	'<div class="rod" id="r2" onclick="myClick(2)">'+stack2+'</div>'+
+	'<div class="rod" id="r3" onclick="myClick(3)">'+stack3+'</div>';
 }
 function myClick(rod){
+	if(currentDisk<=0)
+		takeDisk(rod);
+	else
+		putOnNewRod(rod);
+	drawInitHanoi();
+}
+
+function takeDisk(rod){
 	currentRod = rod;
 	switch(rod) {
 		case 1:
 			
-			if(stack1.length>0){
+			if(stack1.length>0)
 				currentDisk = stack1.pop();
-				document.getElementById("r1").innerHTML =((stack1.length+1) +'<br/>'+ currentDisk );
-			}else{
-				document.getElementById("r1").innerHTML =((stack1.length) +'<br/>'+ 0);
-			}
 			break;
 		case 2:
-			if(stack2.length>0){
+			if(stack2.length>0)
 				currentDisk = stack2.pop();
-				document.getElementById("r2").innerHTML =((stack2.length+1) +'<br/>' +currentDisk);
-			}else{
-				document.getElementById("r2").innerHTML =((stack2.length) +'<br/>'+ 0);
-			}
+				
 			break;
 		case 3:
-			if(stack3.length>0){
+			if(stack3.length>0)
 				currentDisk = stack3.pop();
-				document.getElementById("r3").innerHTML =((stack3.length+1) +'<br/>'+ currentDisk);
-			}else{
-				document.getElementById("r3").innerHTML =((stack3.length) +'<br/>'+ 0);
-			}
 			break;
 		default:
-			document.getElementById("game").innerHTML = "WRONG ROD"
+			document.getElementById("game").innerHTML = "WRONG ROD";
 	}
+}
+function putOnNewRod(rod){
+	switch(rod) {
+		case 1:
+			stack1.push(currentDisk);
+			currentDisk=0;
+			break;
+		case 2:
+			stack2.push(currentDisk);
+			currentDisk=0;
+			break;
+		case 3:
+			stack3.push(currentDisk);
+			currentDisk=0;
+			break;
+		default:
+			document.getElementById("game").innerHTML = "WRONG ROD";
+	}
+	
 }
