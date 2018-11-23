df106bbfb04beebe48ac087c8924e6ae427c3a5a
Konrad Łopuszyński <konrad.lopuszynski@outlook.com>
2018-11-23 17:53:44 +0100

    improve myClick function

--- hanoi.js
+++ hanoi.js
@@ -2,6 +2,9 @@ var stack1 = [], stack2 = [], stack3 = [];
 stack1.push(5); stack1.push(4);stack1.push(3);stack1.push(2);stack1.push(1);
 stack2.push(5); stack2.push(4);stack2.push(3);stack2.push(2);
 stack3.push(5); stack3.push(4);stack3.push(3);
+var currentDisk = 0;
+var currentRod = 0;
+
 function drawHanoi(){
 	document.getElementById("game").innerHTML = 
 	'<div class="rod" id="r1" onclick="myClick(1)"></div>'+
@@ -9,26 +12,35 @@ function drawHanoi(){
 	'<div class="rod" id="r3" onclick="myClick(3)"></div>';
 }
 function myClick(rod){
+	currentRod = rod;
 	switch(rod) {
-    case 1:
-		if(stack1.length>0)
-			document.getElementById("r1").innerHTML =(stack1.length +'<br/>'+ stack1.pop());
-		if(stack1.length==0)
-			document.getElementById("r1").innerHTML =(stack1.length +'<br/>'+ 0);
-        break;
-    case 2:
-		if(stack2.length>0)
-			document.getElementById("r2").innerHTML =(stack2.length +'<br/>'+ stack2.pop());
-		if(stack2.length==0)
-			document.getElementById("r2").innerHTML =(stack2.length +'<br/>'+ 0);
-        break;
-	case 3:
-        if(stack3.length>0)
-			document.getElementById("r3").innerHTML =(stack3.length +'<br/>'+ stack3.pop());
-        if(stack3.length==0)
-			document.getElementById("r3").innerHTML =(stack3.length +'<br/>'+ 0);
-		break;
-    default:
-        document.getElementById("game").innerHTML = "WRONG ROD"
-}
+		case 1:
+			
+			if(stack1.length>0){
+				currentDisk = stack1.pop();
+				document.getElementById("r1").innerHTML =((stack1.length+1) +'<br/>'+ currentDisk );
+			}else{
+				document.getElementById("r1").innerHTML =((stack1.length) +'<br/>'+ 0);
+			}
+			break;
+		case 2:
+			if(stack2.length>0){
+				currentDisk = stack2.pop();
+				document.getElementById("r2").innerHTML =((stack2.length+1) +'<br/>' +currentDisk);
+			}else{
+				document.getElementById("r2").innerHTML =((stack2.length) +'<br/>'+ 0);
+			}
+			break;
+		case 3:
+			if(stack3.length>0){
+				currentDisk = stack3.pop();
+				document.getElementById("r3").innerHTML =((stack3.length+1) +'<br/>'+ currentDisk);
+			}else{
+				document.getElementById("r3").innerHTML =((stack3.length) +'<br/>'+ 0);
+			}
+			break;
+		default:
+			document.getElementById("game").innerHTML = "WRONG ROD"
+	}
+	document.getElementById("game").innerHTML += currentRod;
 }
